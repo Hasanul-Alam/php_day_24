c762a2a5fdda305170e2721a61b566baec3809c7
pavel.cse.buet <shaownakter.1995@gmail.com>
2022-08-16 13:13:08 +0600

    A perfect database

---
+++ pages/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Day-23</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/all.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
+        <a href="#" class="navbar-brand">LOGO</a>
+        <ul class="navbar-nav">
+            <li><a href="action.php?page=home" class="nav-link">Home</a></li>
+            <li><a href="action.php?page=about" class="nav-link">About</a></li>
+            <li class="dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Product Category</a>
+                <ul class="dropdown-menu">
+                        <li><a href="action.php?page=category" class="dropdown-item">Item-1</a></li>
+                </ul>
+            </li>
+            <li><a href="action.php?page=contact" class="nav-link">Contact</a></li>
+            <li><a href="action.php?page=math-operation" class="nav-link">Math Operation</a></li>
+        </ul>
+    </div>
+</nav>
